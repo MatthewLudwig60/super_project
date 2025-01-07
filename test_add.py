1d258e6fb6bf329e62990d7df9af27ee34c16cd9
Matthew Ludwig <hx24112@bristol.ac.uk>
2025-01-07 13:10:35 +0000

    Adding test_add.py to repo

---
+++ test_add.py
@@ -0,0 +1,16 @@
+import pytest
+from add import add_arrays
+@pytest.mark.parametrize("a, b, expect", [
+    ([1, 2, 3],    [4, 5, 6],   [5, 7, 9]),
+    ([-1, -5, -3], [-4, -3, 0], [-5, -8, -3]),
+])
+def test_add_arrays(a, b, expect):
+    output = add_arrays(a, b)
+    assert output == expect
+
+def test_add_arrays_error():
+    a = [1, 2, 3]
+    b = [4, 5]
+    with pytest.raises(ValueError):
+        output = add_arrays(a, b)
+
